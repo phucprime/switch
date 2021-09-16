a31e777861246497af49a08a002469f1a5077c7e
Phuc Nguyen <phuccoder@gmail.com>
2021-09-16 21:08:44 +0700

    re-format & add color modes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
 import { useState } from "react";
 
-function Switch() {
+export const Switch = () => {
   const [toggle, setToggle] = useState(true);
-  const toggleClass = " transform translate-x-5";
+  const toggleClass = " transform translate-x-6";
   return (
     <>
       <div className="flex flex-col justify-center h-screen items-center ">
         {/*   Switch Container */}
 
         <div
-          className="md:w-14 md:h-7 w-12 h-6 flex items-center bg-gray-400 rounded-full p-1 cursor-pointer"
-          onClick={() => {
-            setToggle(!toggle);
-          }}
+          className={
+            (toggle ? "bg-gray-400 " : "bg-green-400 ") +
+            "md:w-14 md:h-7 w-12 h-6 flex items-center rounded-full p-1 cursor-pointer"
+          }
+          onClick={() => setToggle(!toggle)}
         >
           {/* Switch */}
           <div
             className={
+              (toggle ? "bg-black " : "bg-white ") +
               "bg-black md:w-6 md:h-6 h-5 w-5 rounded-full shadow-md transform duration-300 ease-in-out" +
               (toggle ? null : toggleClass)
             }
           ></div>
         </div>
         <div className="font-bold my-4">
-          Hey If you liked it consider following me on twitter{" "}
+          Hey! If you liked it consider following me on twitter{" "}
         </div>
         <div className="bg-blue-600 text-white p-4 rounded-md">
-          <a href="" className="twitter-follow-button" data-show-count="false">
+          <a href="#" className="twitter-follow-button" data-show-count="false">
             Follow @phucprime
           </a>
           <script
@@ -38,6 +40,4 @@ function Switch() {
       </div>
     </>
   );
-}
-
-export default Switch;
+};
